295dd2baf053cc6b0ae9a5a5e9322fc5ed9d4798
Ben Chang <benlchang32@gmail.com>
2024-05-30 16:15:25 -0400

    5/30 - fetch uses headers

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,54 @@
 import React, {useState, useEffect} from 'react';
 
 function App () {
-  const [message, setMessage] = useState('');
+  const [resList, setResList] = useState([]);
 
   useEffect(() => {
-    fetch('/api')
-    .then(response => response.text())
+    fetch('/api', {
+      method: 'GET',
+      headers: {
+        address: '1152 Emmet St N',
+        keywords: 'chicken breast',
+        brand: 'perdue'
+      }
+    })
+    .then(response => response.json())
     .then(data => {
       console.log(data);
-      setMessage(data);
+      
+      setResList(data.data);
+
+      /* PLAN COMPONENTS!
+
+      ShoppingList
+      - parameter: JSON data 
+      - returns: a list of item entries with the store name as a subheader
+
+      ItemInput
+      - parameter: boolean to tell if this should build a list or search one item
+      - returns: a dynamic input field that takes an item and can either add it to a running list via 'add' button and then submit the entire list with a 'submit' button, or just the 'submit' button
+                searches all available stores but can filter based on selection
+      */
+
+      
     })
   }, [])
+
+
   return (
     <div>
-      {message}
+      {resList.map(obj => {
+        return (
+          <>
+            <b>{obj.name}</b>
+            <ul>
+              <li>{obj.store}</li>
+              <li>{obj.address}</li>
+              <li>${obj.price}</li>
+            </ul>
+          </>
+        )
+      })}
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const app = express();
 
+const dataURL = '../data/product_pricing/complete_pricing.csv';
+const fs = require('fs');
+const complete_csv = fs.readFileSync(dataURL).toString();
+
+function csv_to_json(csv, location=null, filter=null, brand=null) {
+    var lines = csv.split('\n')
+    const res = []
+    const headers = lines[0].split(',').map(item => item.trim())
+
+    lines = lines.map((item) => item.split(','))
+    console.log(lines[1])
+
+    if(location){
+        console.log(lines[1][1])
+        lines = lines.filter(item => item[1] == (location))
+        console.log(lines.length)
+    }
+    if(filter){
+        lines = lines.filter((item) => item[2].toLowerCase().includes(filter))
+        console.log(lines.length)
+    }
+    if(brand){
+        lines = lines.filter((item) => item[2].toLowerCase().includes(brand))
+        console.log(lines.length)
+    }
+
+    for(let i = 0; i < lines.length; i++){
+        let temp = lines[i]
+        let obj = {
+            'store' : temp[0],
+            'address': temp[1],
+            'name' : temp[2],
+            'price' : temp[3]
+        }
+        res.push(obj)
+    }
+    return res
+}
+
 app.get('/api', (req, res) => {
-    res.send("Hello from pyERN stack!");
+    let complete_product_pricing = csv_to_json(complete_csv, req.headers.address, req.headers.keywords, req.headers.brand)
+    res.json({'data': complete_product_pricing});
 });
 
 app.listen(5000, () => {
